7710cbf62a5d586949dfe5af0da5bb13dcbd7d9f
sofieberg1 <sofberget@gmail.com>
2024-12-18 20:33:26 +0100

    scoreboard

---
+++ index.js
@@ -0,0 +1,42 @@
+let guestScoreEl = document.getElementById("guest-score-el");
+let homeScoreEl = document.getElementById("home-score-el");
+let homeScore = 0;
+let guestScore = 0;
+
+
+function add1PointHome() {
+    homeScore += 1;
+    homeScoreEl.textContent = homeScore;
+}
+
+function add2PointHome() {
+    homeScore += 2;
+    homeScoreEl.textContent = homeScore;
+}
+
+function add3PointHome() {
+    homeScore += 3;
+    homeScoreEl.textContent = homeScore;
+}
+
+function add1PointGuest() {
+    guestScore += 1;
+    guestScoreEl.textContent = guestScore;
+}
+
+function add2PointGuest() {
+    guestScore += 2;
+    guestScoreEl.textContent = guestScore;
+}
+
+function add3PointGuest() {
+    guestScore += 3;
+    guestScoreEl.textContent = guestScore;
+}
+
+function newGame() {
+    homeScore = 0;
+    guestScore = 0;
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+}
